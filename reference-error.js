fa1432e0d4c267e25b8c07349426924bbefcc317
evo264714 <mh264714@gmail.com>
2023-03-06 17:08:02 +0600

    sec

---
+++ reference-error.js
@@ -0,0 +1,22 @@
+const x = "Evo"
+const y = "Hasan"
+// console.log(z);
+
+
+// console.log(name);
+// const name = "Jalil";
+// let name = "Jalil";
+// var name = "Jalil";
+
+// console.log(add2(2, 3));
+function add(n1, n2){
+    return n1 + n2;
+}
+
+const add2 = (n1, n2) => n1 + n2;
+
+
+function doSomething(){
+    const sum = 55;
+}
+// console.log(sum);
---
+++ type-error.js
@@ -0,0 +1,15 @@
+const num = 5;
+
+// num.push(45);
+
+// let address;
+// console.log(address.home);
+let address = {
+    city: 'Dhaka',
+    postcode: 4560
+};
+// console.log(address.home.building);
+console.log(address?.home?.building);
+
+const a = 'Evo';
+// a = 'Evo1';
