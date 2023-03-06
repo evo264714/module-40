d23df3645022d48be5af405fd80b6668c39f42f9
evo264714 <mh264714@gmail.com>
2023-03-06 19:26:58 +0600

    done

---
+++ test.js
@@ -0,0 +1,26 @@
+function text(){
+    var str = "Hello";
+    return str;
+}
+
+ggg();
+// // console.log(str);
+// const num = 10;
+// num();
+// let x = 10;
+// x = y + 5;
+// console.log(x);
+
+// const obj = { name: 'John' };
+// Object.keys(obj).forEach(prop => console.log(prop));
+
+// const name = 'John';
+// name.toUpperCase = () => 'JOHN';
+// // console.log(name.toUpperCase());
+// const arr = [1, 2, 3];
+// arr.slice(-2, 0);
+// console.log(arr);
+
+
+// const greeting = 'Hello, world!';
+// console.log(greeting);
